449303d2bddaa0196878a62c61fc7c5e81fd3b7c
Kristen Thelen <kristen.thelen@landesk.com>
2018-02-08 12:24:42 -0700

    Constrain the monster to the canvas

---
+++ Lesson03_Canvas_with_if_else_AFTER/js/game.js
@@ -0,0 +1,93 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "images/background.png";
+
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+
+// Update game objects
+var update = function (modifier) {
+	if (38 in keysDown && hero.y > 10) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	else if (40 in keysDown && hero.y < 420) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown && hero.x > 10) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown && hero.x < 455) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+
+};
+
+// Draw everything
+var render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0, 512, 480);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y, 50, 50);
+	}
+};
+
+// The main game loop
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
+
+// Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+var then = Date.now();
+// Let's play this game!
+hero.x = canvas.width / 2;
+hero.y = canvas.height / 2;
+main();
