17149c0ee1f5624a6fb03899280262b341691df8
Kristen Thelen <kristen.thelen@landesk.com>
2018-03-01 16:31:17 -0700

    move drawBorder and setBackgroundImage into the myGameArea object

--- CanvasGame1.GameCanvas/js/game.js
+++ CanvasGame1.GameCanvas/js/game.js
@@ -11,24 +11,21 @@ var myGameArea = {
         this.canvas.width = this.canvasWidth;
         this.canvas.height = this.canvasHeight;
         this.context = this.canvas.getContext("2d");
-        drawBorder();
-        setBackgroundImage();
+        this.drawBorder();
+        this.setBackgroundImage();
         document.body.appendChild(this.canvas);
+    },
+    setBackgroundImage: function() {
+      var background = new Image();
+      background.src = "images/beach.jpg";
+      background.onload = function(){
+            myGameArea.context.drawImage(background,0,0);
+          }
+      },
+    drawBorder: function() {
+      myGameArea.context.rect(0, 0, myGameArea.canvasWidth, myGameArea.canvasHeight);
+    	myGameArea.context.stroke();
     }
-}
-
-function drawBorder() {
-  myGameArea.context.rect(0, 0, myGameArea.canvasWidth, myGameArea.canvasHeight);
-	myGameArea.context.stroke();
-}
-
-function setBackgroundImage() {
-  var background = new Image();
-  background.src = "images/beach.jpg";
-
-  background.onload = function(){
-  myGameArea.context.drawImage(background,0,0);
-  }
 }
 
 window.onload = function() {
