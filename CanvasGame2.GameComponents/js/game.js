54549fe49daf2988de262510ddcc4ab3e3a97a48
Kristen Thelen <kristen.thelen@landesk.com>
2018-03-15 10:27:42 -0600

    Refactor based on what was done in class.

--- CanvasGame2.GameComponents/js/game.js
+++ CanvasGame2.GameComponents/js/game.js
@@ -1,56 +1,55 @@
 function startGame() {
-    myGameArea.start();
+  myGameArea.start();
 }
 
-function component(width, height, x, y) {
+function component(width, height, x, y, imageSrc) {
   this.width = width;
   this.height = height;
   this.x = x;
-  this.y = y
+  this.y = y;
   this.componentImage = new Image();
-  this.componentImage.src = "images/bird.png";
+  this.componentImage.src = imageSrc;
 }
-
 
 function updateGameArea() {
   myGameArea.clear();
+  myGameArea.component.x += 1;
   myGameArea.update();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    component: new component(50, 50, 10, 120),
-    context : null,
-    canvasWidth : 480,
-    canvasHeight : 270,
-    background : new Image(),
-    start : function() {
-        this.canvas.width = this.canvasWidth;
-        this.canvas.height = this.canvasHeight;
-        this.context = this.canvas.getContext("2d");
-        this.drawBorder();
-        this.setBackgroundImage();
-        document.body.appendChild(this.canvas);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    setBackgroundImage: function() {
-      this.background.src = "images/beach.jpg";
-      ctx = this.context;
-      component = this.component;
-      },
-    drawBorder: function() {
-      this.context.rect(0, 0, this.canvasWidth, this.canvasHeight);
-    	this.context.stroke();
-    },
-    clear: function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    update: function() {
-      ctx.drawImage(this.background,0,0);
-      ctx.drawImage(component.componentImage, component.x, component.y, component.width, component.height);
-    }
+  canvas: document.createElement("canvas"),
+  component: new component(50, 50, 10, 120, "images/bird.png"),
+  context: null,
+  canvasWidth: 480,
+  canvasHeight: 270,
+  background: new Image(),
+  start: function() {
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
+    this.context = this.canvas.getContext("2d");
+    this.drawBorder();
+    this.setBackgroundImage();
+    document.body.appendChild(this.canvas);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  setBackgroundImage: function() {
+    component = this.component;
+    this.background.src = "images/beach.jpg";
+  },
+  clear: function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  update: function() {
+    this.context.drawImage(this.background, 0, 0, myGameArea.canvasWidth, myGameArea.canvasHeight);
+    this.context.drawImage(component.componentImage, component.x, component.y, component.width, component.height);
+  },
+  drawBorder: function() {
+    myGameArea.context.rect(0, 0, myGameArea.canvasWidth, myGameArea.canvasHeight);
+    myGameArea.context.stroke();
+  }
 }
 
 window.onload = function() {
-    startGame();
+  startGame();
 }
